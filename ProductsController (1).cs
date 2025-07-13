33140991385edca51e3c4b27c37689ea6cb76d62
AKSHAYA TV <akshaya29052005@gmail.com>
2025-07-13 22:44:46 +0530

    Add files via upload

---
+++ ProductsController (1).cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ProductsController : ControllerBase
+{
+    private static readonly List<Product> _products = new();
+
+    [HttpGet]
+    public IEnumerable<Product> Get() => _products;
+
+    [HttpPost]
+    public IActionResult Post(Product p) { _products.Add(p); return CreatedAtAction("Get", new { id = p.Id }, p); }
+    // Add PUT & DELETE similarly
+}
